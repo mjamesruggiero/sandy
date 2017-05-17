a06616cab1c7c4a641c3049061e3a83202c4c8e9
mjamesruggiero <mjamesruggiero@gmail.com>
2017-05-16 17:16:16 -0700

    S3 buckets without policies can be saved to CSV
    * TODO unit tests

--- src/clj/sandy/aws/s3.clj
+++ src/clj/sandy/aws/s3.clj
@@ -1,33 +1,68 @@
 (ns sandy.aws.s3
-  (:require [amazonica.aws.s3 :as s3]))
+  (:require [amazonica.aws.s3 :as s3]
+            [sandy.utils :as utils])
+  (:import com.amazonaws.services.s3.model.AmazonS3Exception))
 
 (defn- fetch-lifecycle
   "Get the S3 lifecycle configuration for a bucket
   and return map of bucket name and lifecycle"
   [bucket]
-  {:name bucket
-   :lifecycle (s3/get-bucket-lifecycle-configuration bucket)})
+  (let [lifecycle
+        (try
+          (s3/get-bucket-lifecycle-configuration bucket)
+          (catch AmazonS3Exception se {:error (:cause se)})
+          (finally (println (str "failed to get bucket lifecycle for " bucket))))]
+    {:name      bucket
+     :lifecycle lifecycle}))
 
 (defn fetch-buckets
-  "Fetch all buckets and lifecycles"
+  "Fetch all buckets and lifecycles; side-effecting"
   []
   (let [names (map :name (s3/list-buckets))]
     (map fetch-lifecycle names)))
 
 (defn has-lifecycle?
   "Is the lifecycle rule set populated?"
-  [lc]
-  (not (empty? (:rules lc))))
+  [m]
+  (not (nil? (:lifecycle m))))
 
 (defn with-lifecycle-rules
   [buckets]
   (filter
    (fn [m]
-     (has-lifecycle? (:lifecycle m)))
+     (has-lifecycle? m))
    buckets))
+
+(defn split-with-lifecycle
+  "Given a seq of bucket maps,
+  returns a tuple of buckets with lifecycles
+  defined and buckets with undefined lifecycles"
+  [buckets]
+  ((juxt filter remove) has-lifecycle? buckets))
 
 (defn flatten-rule
   [rule]
   (let [s (get-in rule [:transition :storage-class-as-string])
         d (get-in rule [:transition :days])]
     {:storage-class s :days d}))
+
+(defn size
+  "Given bucket metadata, return size
+  of all assets inside the bucket"
+  [bucket]
+  (let [ms (second (second bucket))]
+    (reduce + 0 (map :size ms))))
+
+(defn- decorate-with-size
+  "Side-effecting"
+  [m]
+  (let [bucket-metadata (s3/list-objects :bucket-name (:name m))]
+    (assoc m :size (size bucket-metadata))))
+
+(defn- save-buckets-without-policies
+  "Side-effecting"
+  [filepath]
+  (let [all-buckets (fetch-buckets)
+        without-policies (second (split-with-lifecycle all-buckets))
+        s (map decorate-with-size without-policies)]
+    (utils/maps->csv filepath s [:name :size])))
--- test/clj/sandy/test/aws/s3.clj
+++ test/clj/sandy/test/aws/s3.clj
@@ -23,27 +23,28 @@
   (testing "given a seq of buckets, returns only those with lifecycle rules"
     (is (= 2 (count (sut/with-lifecycle-rules fake-buckets))))))
 
+(def fake-bucket-map
+  {:name    "fake-bucket-name"
+   :lifecycle {:rules [{:id                                    "tf-s3-lifecycle-p45rdatavvguzatnctp6i3djju"
+                        :status                                "Enabled"
+                        :transition                            {:storage-class-as-string "STANDARD_IA"
+                                                                :storage-class           {}
+                                                                :days                    30}
+                        :prefix                                ""
+                        :expiration-in-days                    -1
+                        :transitions                           [{:storage-class-as-string "STANDARD_IA"
+                                                                 :storage-class           {}
+                                                                 :days                    30}]
+                        :expired-object-delete-marker?         false
+                        :noncurrent-version-expiration-in-days -1}]}})
+
 (deftest has-lifecycle-populated-returns-true-for-populated-rule
   (testing "Given a rule that is populated, returns true"
-    (let [fake-lifecycle
-          {:rules
-           [{:id                            "tf-s3-lifecycle-nss4gdxzinaota2imfbyl75tmy",
-             :prefix                        "",
-             :transition                    {:storage-class           {},
-                                             :days                    90,
-                                             :storage-class-as-string "STANDARD_IA"},
-             :status                        "Enabled",
-             :expiration-in-days            -1,
-             :transitions                   [{:storage-class           {},
-                                              :days                    90,
-                                              :storage-class-as-string "STANDARD_IA"}],
-             :expired-object-delete-marker? false}]
-           }]
-      (is (true? (sut/has-lifecycle? fake-lifecycle))))))
+      (is (true? (sut/has-lifecycle? fake-bucket-map)))))
 
 (deftest is-rule-populated-returns-false-for-empty-rule
   (testing "Given unpopulated rule, returns false"
-    (let [fake-lifecycle {:rules []}]
+    (let [fake-lifecycle {:name "fake-bucket-name" :lifecycle nil}]
       (is (false? (sut/has-lifecycle? fake-lifecycle))))))
 
 (deftest can-flatten-lifecycle-rule
